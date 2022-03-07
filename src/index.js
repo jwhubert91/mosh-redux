e468406863d0436142d093a13bfd32c0f579d7aa
James Hubert <jwhubert91@gmail.com>
2022-03-06 21:30:41 -0800

    Final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+function sayHello() {
+  return function() {
+    return "Hello World";
+  }
+}
+
+// The purpose of the below assignment and calls is to demonstrate that functions can be treated like any other object in JS
+let fn = sayHello();
+let message = fn();
+
+
+/*
+
+let fn = sayHello();
+
+// The following two functions return the same thing
+
+fn();
+sayHello();
+
+// Functions can also be passed to other functions
+
+function greet(fnMessage) {
+  console.log(fnMessage())
+}
+
+greet(sayHello);
+
+*/
